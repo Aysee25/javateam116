5fb695a99037f81e4a2daa192f68c2d56e7f4257
Aysee25 <aysee25@gmail.com>
2023-05-04 13:01:35 +0300

    first

--- src/day25_inheritance/Araba.java
+++ src/day25_inheritance/Araba.java
@@ -1,12 +1,11 @@
-package day24_inheritance;
+package day25_inheritance;
 
-public class DAraba {
-    DAraba(){
+public class Araba {
+    Araba(){
         System.out.println("Araba constructor calisti");
     }
     String model="Arabalarin modeli olur";
     String motor= "Arabalarda motor olur";
     String marka="Arabalarda marka olur";
-
-
 }
+
