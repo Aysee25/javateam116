8d85352b8f32a0d10622a7e09f86b20b1dc2d12e
Aysee25 <aysee25@gmail.com>
2023-05-10 17:32:39 +0300

    guncel

---
+++ src/day27_Polymorphism_abstractClasses/C01_overridingReturnType.java
@@ -0,0 +1,22 @@
+package day27_Polymorphism_abstractClasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C01_overridingReturnType {
+    void method1() {
+
+    }
+
+    double method2() {
+        return 0;
+    }
+
+    String method3() {
+        return "Java Candir";
+    }
+
+    List<String> method4() {
+        return new ArrayList<>();
+    }
+}
---
+++ src/day27_Polymorphism_abstractClasses/C06_AbstracGP.java
@@ -0,0 +1,17 @@
+package day27_Polymorphism_abstractClasses;
+
+public abstract class C06_AbstracGP {
+    abstract void method1();
+
+    abstract void method2();
+
+    void method3(){
+        System.out.println("GP concrete method3");
+    }
+    void method4(){
+        System.out.println("GP concrete method4");
+    }
+
+
+
+}
