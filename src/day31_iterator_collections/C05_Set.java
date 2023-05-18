79f39d4bff60936c5065b140f7b1c86480623c43
Aysee25 <aysee25@gmail.com>
2023-05-18 14:25:58 +0300

    sp

--- src/day31_iterator_collections/C05_Set.java
+++ src/day31_iterator_collections/C05_Set.java
@@ -1,8 +1,19 @@
 package day31_iterator_collections;
 
+import com.sun.source.doctree.SeeTree;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class C05_Set {
     public static void main(String[] args) {
 
+        Set<String> sinifList = new HashSet<>();
+        sinifList.add("Sara");
+        sinifList.add("Sevda");
+        System.out.println(sinifList); // [Sevda, Sara]
+        sinifList.add("Sevda");
+        System.out.println(sinifList); // [Sevda, Sara]
 
     }
 }
